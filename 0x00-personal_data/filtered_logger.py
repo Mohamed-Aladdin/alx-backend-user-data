37379c23bf0d1afd07dc321807a393da13590662
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-08-28 23:26:42 +0300

    Fix typo

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,18 +37,15 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the database
     """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
-    )
-    return connection
+    username = os.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host_name = os.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.get("PERSONAL_DATA_DB_NAME")
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host_name,
+                                                     database=db_name)
+    return cnx
 
 
 def main():
