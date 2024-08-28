6954729734d27a7474860cb5e663423e03dce448
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-08-28 23:08:39 +0300

    Fix typo

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,18 +14,7 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """
-    Replaces sensitive information in a message with a redacted value
-    based on the list of fields to redact
-
-    Args:
-        fields: list of fields to redact
-        redaction: the value to use for redaction
-        message: the string message to filter
-        separator: the separator to use between fields
-
-    Returns:
-        The filtered string message with redacted values
+    """returns a logging.Logger object
     """
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
